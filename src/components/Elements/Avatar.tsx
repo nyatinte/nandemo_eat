8c63077c65e315e9513decabaa854838c789326a
nyatinte <takumi.062757@gmail.com>
2023-04-17 21:51:23 +0900

    スタイルの作成、コンポーネントの作成

---
+++ src/components/Elements/Avatar.tsx
@@ -0,0 +1,32 @@
+import { memo, type ComponentProps, type FC, useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+
+type Size = "xs" | "sm" | "md" | "lg" | "xl";
+type AvatarProps = ComponentProps<"img"> & {
+  size?: Size;
+};
+export const Avatar: FC<AvatarProps> = ({ src, size, ...props }) => {
+  const sizeClass = useMemo(() => {
+    switch (size) {
+      case "xs":
+        return "w-6";
+      case "sm":
+        return "w-8";
+      case "md":
+        return "w-12";
+      case "lg":
+        return "w-16";
+      case "xl":
+        return "w-24";
+      default:
+        return "w-12";
+    }
+  }, [size]);
+  return (
+    <div className="avatar">
+      <div className={twMerge("rounded-full", sizeClass)}>
+        <img src={src} alt="avatar" {...props} />
+      </div>
+    </div>
+  );
+};
