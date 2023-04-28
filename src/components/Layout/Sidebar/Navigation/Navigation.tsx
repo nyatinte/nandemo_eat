ce830b1d24f99af50f29c2fcff242146e7fadc31
nyatinte <takumi.062757@gmail.com>
2023-04-28 17:25:04 +0900

    未表示の場合にクリックできないように修正

--- src/components/Layout/Sidebar/Navigation/Navigation.tsx
+++ src/components/Layout/Sidebar/Navigation/Navigation.tsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import { MenuItem } from "../MenuItem";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { Link } from "@/components/Elements";
+import { useAtomValue } from "jotai";
+import { sidebarOpenAtom } from "@/atoms/sidebarOpenAtom";
 
 const variants = {
   open: {
@@ -17,8 +19,13 @@ const variants = {
  */
 export const Navigation: FC = () => {
   const user = useUser();
+  const isSidebarOpen = useAtomValue(sidebarOpenAtom);
   return (
-    <motion.ul variants={variants} className="absolute top-24 w-72 p-6 ">
+    <motion.ul
+      variants={variants}
+      className="absolute top-24 w-72 p-6"
+      style={{ pointerEvents: isSidebarOpen ? "auto" : "none" }}
+    >
       <MenuItem>
         <Link href="/" label="Home" />
       </MenuItem>
