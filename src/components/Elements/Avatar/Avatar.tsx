28b34a05a15a631be19a12037dba72d2df0ce492
nyatinte <takumi.062757@gmail.com>
2023-04-28 17:24:24 +0900

    NextjsのImageに変更

--- src/components/Elements/Avatar/Avatar.tsx
+++ src/components/Elements/Avatar/Avatar.tsx
@@ -1,31 +1,22 @@
-import { memo, type ComponentProps, type FC, useMemo } from "react";
-import { twMerge } from "tailwind-merge";
+import Image, { type ImageProps } from "next/image";
+import { type FC } from "react";
 
-type Size = "xs" | "sm" | "md" | "lg" | "xl";
-export type AvatarProps = ComponentProps<"img"> & {
-  size?: Size;
-};
-export const Avatar: FC<AvatarProps> = ({ src, size, ...props }) => {
-  const sizeClass = useMemo(() => {
-    switch (size) {
-      case "xs":
-        return "w-6";
-      case "sm":
-        return "w-8";
-      case "md":
-        return "w-12";
-      case "lg":
-        return "w-16";
-      case "xl":
-        return "w-24";
-      default:
-        return "w-12";
-    }
-  }, [size]);
+export type AvatarProps = Omit<ImageProps, "alt">;
+export const Avatar: FC<AvatarProps> = ({ src, ...props }) => {
   return (
     <div className="avatar">
-      <div className={twMerge("rounded-full", sizeClass)}>
-        <img src={src} alt="avatar" {...props} />
+      <div className="h-12 w-12 rounded-full">
+        {src !== undefined ? (
+          <Image src={src} alt="avatar" {...props} width={48} height={48} />
+        ) : (
+          <Image
+            src="/noimage_user.png"
+            alt="avatar"
+            {...props}
+            width={48}
+            height={48}
+          />
+        )}
       </div>
     </div>
   );
