be73729b2db7d73808e0279b8a15c3eb98b86283
nyatinte <takumi.062757@gmail.com>
2023-05-07 20:28:00 +0900

    eslintの設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,27 +3,81 @@ const path = require("path");
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [{
-    extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
-    files: ["*.ts", "*.tsx"],
-    parserOptions: {
-      project: path.join(__dirname, "tsconfig.json")
-    }
-  }],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: path.join(__dirname, "tsconfig.json")
+    project: path.join(__dirname, "tsconfig.json"),
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  plugins: ["@typescript-eslint", "import", "unused-imports"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
   rules: {
-    "@typescript-eslint/consistent-type-imports": ["warn", {
-      prefer: "type-imports",
-      fixStyle: "inline-type-imports"
-    }],
-    "@typescript-eslint/no-unused-vars": ["warn", {
-      argsIgnorePattern: "^_"
-    }]
-  }
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "react/jsx-max-depth": [
+      "warn",
+      {
+        max: 5,
+      },
+    ],
+    "import/no-unresolved": "off",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always", // import groups の間 1行あける
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
 };
 module.exports = config;
