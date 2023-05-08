37feff4d92bd787f2cc424eb1bc7f028fc68249a
nyatinte <takumi.062757@gmail.com>
2023-05-08 23:00:57 +0900

    sitemapを追加

---
+++ sitemap.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("next-sitemap").IConfig}*/
+const config = {
+  siteUrl: "https://nandemo-eat.vercel.app/",
+  changefreq: "weekly",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: "./out",
+};
+
+export default config;
