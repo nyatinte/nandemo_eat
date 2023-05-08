1b27ecd8be2b78e297fcdcaab5c37997dbda809e
nyatinte <takumi.062757@gmail.com>
2023-05-08 15:38:54 +0900

    テーマスタイルを変更

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,25 @@ export default {
       },
       maxHeight: {
         "without-header": "calc(100vh - 80px)",
-      }
+      },
     },
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fef08a",
+          secondary: "#bef264",
+          accent: "#67e8f9",
+          neutral: "#d1d5db",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
 } satisfies Config;
