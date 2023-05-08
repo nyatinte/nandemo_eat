036fb85de38ee2e9f4a4d27b9ea810f2f7da4a95
nyatinte <takumi.062757@gmail.com>
2023-05-08 14:47:45 +0900

    ステップを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { motion } from "framer-motion";
 import { useAtom } from "jotai";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useMemo } from "react";
+import { twMerge } from "tailwind-merge";
 
-import { sectionAtom } from "@/atoms/sectionAtom";
+import { type Section, sectionAtom } from "@/atoms/sectionAtom";
 import {
   HeroSection,
   CategorySection,
@@ -31,6 +33,25 @@ const Home: NextPage = () => {
     }
   }, [sectionIndex]);
 
+  const SectionStep = useMemo(() => {
+    const steps: Section[] = ["category", "subCategory", "dish"];
+    const index = steps.findIndex((step) => step === sectionIndex);
+    return (
+      <motion.ul
+        className="steps"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <li className="step-primary step">カテゴリ</li>
+        <li className={twMerge("step", index >= 1 && "step-primary")}>
+          サブカテゴリ
+        </li>
+        <li className={twMerge("step", index >= 2 && "step-primary")}>料理</li>
+      </motion.ul>
+    );
+  }, [sectionIndex]);
+
   useEffect(() => {
     setSection("hero");
   }, [setSection]);
@@ -45,6 +66,9 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {sectionIndex !== "hero" && (
+        <div className="grid place-content-center px-4 py-2">{SectionStep}</div>
+      )}
       <Section />
     </>
   );
