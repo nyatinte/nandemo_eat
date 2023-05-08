4ca99c379f57c37cc330a6ce39914da3530c4aa7
nyatinte <takumi.062757@gmail.com>
2023-05-08 23:42:44 +0900

    設定を公式に従って変更

--- sitemap.config.mjs
+++ sitemap.config.mjs
@@ -1,10 +1,14 @@
-/** @type {import("next-sitemap").IConfig}*/
+/**
+ * @type {import("next-sitemap").IConfig}
+ * @see https://github.com/iamvishnusankar/next-sitemap#create-config-file
+ */
 const config = {
-  siteUrl: "https://nandemo-eat.vercel.app/",
+  siteUrl: process.env.SITE_URL || "https://nandemo-eat.vercel.app/",
   changefreq: "weekly",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: "public",
+  generateIndexSitemap: false,
 };
 
 export default config;
